7d4e988726a6fdfa9fb86ed427b97543b17bdfb3
Denis SIMON SONEIRA <dsimonsoneira.ext@orange.com>
2016-10-29 13:52:05 +0200

    add crypted password

--- src/main/java/co/simplon/exercise/core/service/business/UserService.java
+++ src/main/java/co/simplon/exercise/core/service/business/UserService.java
@@ -3,6 +3,9 @@ package co.simplon.exercise.core.service.business;
 import co.simplon.exercise.core.model.User;
 import co.simplon.exercise.core.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +17,13 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    @Qualifier("passwordEncoder")
+    private PasswordEncoder passwordEncoder;
+
     @Transactional
     public void addOrUpdateUserAndRoles(String username, String password, boolean enabled, User.Role role) {
-        userRepository.save(new User(username, password, role, enabled));
+        userRepository.save(new User(username, passwordEncoder.encode(password), role, enabled));
     }
 
     @Transactional(readOnly = true)
